82a6d7f052bde25bdbec0c2ecc1c72ad977de6cc
Chander Parkash <chanderparkash179@gmail.com>
2023-12-28 14:48:22 +0500

    added db configuration

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const colors = require("colors")
+const colors = require("colors");
+const { mongoConnect } = require("./src/database/connection")
 
-dotenv.config({ path: './configurations/.env' });
+// .ENV configuration
+dotenv.config({ path: './src/configurations/.env' });
 
+// port declaration
 const PORT = process.env.PORT || 8080;
+// app initialization
 const app = express();
+
+// connect mongodb
+mongoConnect(process.env.DB_LOCAL_URL).then(() =>
+  console.log(`database connection connected successfully!`.bgYellow.black)
+);
 
 
 app.listen(PORT, () => console.log(`server started in ${process.env.DEV_MODE} Mode on PORT => ${PORT}`.bgCyan.black));

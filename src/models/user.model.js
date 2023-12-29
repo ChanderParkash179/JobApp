b87e836c2ade8723e03f758cfaec9a753392f02a
Chander Parkash <chanderparkash179@gmail.com>
2023-12-29 15:15:44 +0500

    password hashing

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   firstName: {
@@ -25,6 +26,13 @@ const userSchema = new mongoose.Schema({
     default: "Pakistan"
   }
 }, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
 
 const user = mongoose.model("user", userSchema);
 
